1d1a5524501c3fbe97f8260a9ab1a8c2c2caa4a2
James Shi <jshi@live.ca>
2018-09-08 22:13:35 -0700

    room search

--- frontend/src/navigation.js
+++ frontend/src/navigation.js
@@ -1,25 +1,52 @@
 import React from "react";
-import { Label, Menu } from "semantic-ui-react";
+import { Search, Label, Menu } from "semantic-ui-react";
+import _ from "lodash";
 
 import Input from "./input";
 import { withContext, RoomContext } from "./context";
 
 class Navigation extends React.PureComponent {
+    state = {
+        results: []
+    }
+
+    onSearchChange = async (event, {value}) => {
+        if (!value) {
+            return;
+        }
+
+        const encodedRoomName = encodeURIComponent(value);
+        const response = await fetch(`/api/rooms?name=${encodedRoomName}`);
+        if (response.ok) {
+            const rooms = await response.json()
+            const results = rooms.map(room => ({ id: room.id, title: room.name }))
+            this.setState({results})
+        }
+    }
+
+    onResultSelect = (event, { result }) => {
+        const { switchRoom } = this.props;
+        switchRoom({id: result.id, name: result.title});
+    }
+
     render() {
         const { addRoom, rooms, switchRoom, currentRoom } = this.props;
         return (
             <Menu pointing fluid vertical>
                 <Menu.Item>
-                    <Input
-                        submitButtonIcon="add"
-                        placeholder="room name"
-                        onSubmit={addRoom}
+                    <Search 
+                        fluid 
+                        noResultsMessage="no results found"
+                        placeholder="Search rooms"
+                        onSearchChange={_.debounce(this.onSearchChange, 500)}
+                        onResultSelect={this.onResultSelect}
+                        results={this.state.results}
                     />
                 </Menu.Item>
                 {rooms.map((room, index) => (
                     <Menu.Item
                         name={room.name}
-                        active={currentRoom === room}
+                        active={currentRoom && room && currentRoom.id === room.id}
                         key={index}
                         onClick={() => switchRoom(room)}
                     />
